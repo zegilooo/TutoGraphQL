527c912448c7e46545f5646a8aed0025f7b1dd9b
Gilles Eschenbrenner <gilles.eschenbrenner@ekino.com>
2017-04-18 15:36:33 +0200

    myStep1 finished

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,9 +1,15 @@
 const {
     GraphQLSchema,
-    GraphQLObjectType
+    GraphQLObjectType,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLInt
 } = require('graphql');
 
+const _ = require ('lodash');
+
 const personType = require('../graphql/personType');
+const personsArray = require('../data/persons.json').persons;
 
 module.exports = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -12,6 +18,22 @@ module.exports = new GraphQLSchema({
             me: {
                 type:    personType,
                 resolve: () => require('../data/me')
+            },
+            persons: {
+                type:    new GraphQLList(personType),
+                description: 'List of persons',
+                resolve: () => personsArray
+            },
+            person: {
+                type:    personType,
+                  args: {
+                      id: {
+                          name: 'id',
+                          type: new GraphQLNonNull(GraphQLInt)
+                      }
+                  },
+                  resolve: (root, {id}) => _.find(personsArray, person => person.id === id)
+
             }
         }
     })
